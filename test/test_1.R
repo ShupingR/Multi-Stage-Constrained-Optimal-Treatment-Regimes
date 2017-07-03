48a9fb3852fa6051109801ed8607d0b5cb075862
Shuping <shuping.ruan@gmail.com>
2017-07-03 15:14:19 -0400

    test update

--- test/test_1.R
+++ test/test_1.R
@@ -1,9 +1,7 @@
-library('R.matlab') # call matlab from R
 library('FlexCoDE') # conditional density estimation
 library('stats') # univariate normal
 library('MASS') # multivariate noraml
 library('iqLearn')# iqLearn
-library('ggplot2')
 library('nloptr')
 set.seed(2017)
 
@@ -16,160 +14,98 @@ int <- rep(1, n) # intercept
 muX1 <- 1 # mean
 sigmaX1 <- 1 # covariance 
 X1 <- rnorm(n, mean = muX1, sd = sigmaX1)
-H1 <- data.frame(int, X1)
 A1 <- sample(0:1, n, replace=TRUE)
 A1[A1 == 0] <- -1
+muEpX2 = 0
+SigmaEpX2 = 1
+EpX2 <- rnorm(n, muEpX2, SigmaEpX2)
+H1 <- data.frame(int, X1)
 X2Beta0 <- c(0.5, 0.75)
 X2Beta1 <- c(0.25, 0.5)
 dim(X2Beta0) <- c(2,1)
 dim(X2Beta1) <- c(2,1)
-muEpX2 = 0
-SigmaEpX2 = 1
-EpX2 <- rnorm(n, muEpX2, SigmaEpX2)
 X2 = as.matrix(H1) %*% X2Beta0 + A1 * (as.matrix(H1) %*% X2Beta1) + EpX2 
-int2 <- int
-H2 = data.frame(int2, X2)
-Ybeta0 = c(30, 3);
-Ybeta1 = c(5, -1.5);
-Zbeta0 = c(15, 1);
-Zbeta1 = c(3, -0.5);
-dim(Ybeta0) <- c(2,1)
-dim(Ybeta1) <- c(2,1)
-dim(Zbeta0) <- c(2,1)
-dim(Zbeta1) <- c(2,1)
-muEpYZ = c(0, 0);
-SigmaEpYZ = matrix(c(1 , 0.7, 0.7, 1), 2, 2)
+H2 <- data.frame(int, X1, A1, X2)
 A2 <- sample(0:1, n, replace=TRUE)
 A2[A2 == 0] <- -1
+muEpYZ = c(0, 0);
+SigmaEpYZ = matrix(c(1, 0.7, 0.7, 1), 2, 2)
 EpYZ =  mvrnorm(n, muEpYZ, SigmaEpYZ)
 EpY = EpYZ[,1]
 EpZ = EpYZ[,2]
+Ybeta0 = c(5, 3, 1, 10);
+Ybeta1 = c(5, 6, 2, 15);
+Zbeta0 = c(5, 1.5, 1, 5);
+Zbeta1 = c(5, 3, 2, 7.5);
+dim(Ybeta0) <- c(2,1,2,1)
+dim(Ybeta1) <- c(2,1,2,1)
+dim(Zbeta0) <- c(2,1,2,1)
+dim(Zbeta1) <- c(2,1,2,1)
 Y = as.matrix(H2) %*% Ybeta0 + A2 * (as.matrix(H2) %*% Ybeta1) + EpY
 Z = as.matrix(H2) %*% Zbeta0 + A2 * (as.matrix(H2) %*% Zbeta1) + EpZ
 yzData <- data.frame(Y,Z)
-firstStageCovars <- data.frame(H1,A1)
-secondStageCovars <- data.frame(H2, A2)
-fullHistoryCovars <- data.frame(H2, A2, H1, A1)
-#ggplot(data, aes(x=Y, y=Z)) + 
-#  geom_point() + 
-#
-#geom_rug(col="steelblue",alpha=0.1, size=1.5)
+L <- data.frame(Y, Z, A2, X2, A1, X1)
 
-# generate data
-# determine sample sizes
 nTrain=round(0.7*n)
-nValidation=round(0.25*n)
-nTest=n-nTrain-nValidation
+nValidat=round(0.25*n)
+nTest=n-nTrain-nValidat
 
 # split data
 randomIndex=sample(1:n)
+LTrain = L[randomIndex[1:nTrain],]
+LValidat = L[randomIndex[(nTrain+1):(nTrain+nValidat)],]
+LTest = L[randomIndex[(nTrain+nValidat+1):n],]
 
-yzTrain=yzData[randomIndex[1:nTrain],]
-yzValidation=yzData[randomIndex[(nTrain+1):(nTrain+nValidation)],]
-yzTest=yzData[randomIndex[(nTrain+nValidation+1):n],]
-
-firstStageTrain=firstStageCovars[randomIndex[1:nTrain],]
-firstStageValidation=firstStageCovars[randomIndex[(nTrain+1):(nTrain+nValidation)],]
-firstStageTest=firstStageCovars[randomIndex[(nTrain+nValidation+1):n],]
-
-secondStageTrain=secondStageCovars[randomIndex[1:nTrain],]
-secondStageValidation=secondStageCovars[randomIndex[(nTrain+1):(nTrain+nValidation)],]
-secondStageTest=secondStageCovars[randomIndex[(nTrain+nValidation+1):n],]
-
-fullHistoryTrain=fullHistoryCovars[randomIndex[1:nTrain],]
-fullHistoryValidation=fullHistoryCovars[randomIndex[(nTrain+1):(nTrain+nValidation)],]
-fullHistoryTest=fullHistoryCovars[randomIndex[(nTrain+nValidation+1):n],]
+L2Train = as.matrix(LTrain[,c("X2","A2", "X1", "A1")])
+L2Validat = as.matrix(LValidat[,c("X2","A2", "X1", "A1")])
+L2Test = as.matrix(LTest[,c("X2","A2", "X1", "A1")])
 
 # Fit sparse additive FlexCoDE, Y on full history 
-fit_y_history=fitFlexCoDE(fullHistoryTrain[,c(2:3,5:6)],yzTrain[,1],
-                          fullHistoryValidation[,c(2:3,5:6)],yzValidation[,1],
-                          fullHistoryTest[,c(2:3,5:6)],yzTest[,1],nIMax = 30,
-                          regressionFunction = regressionFunction.SpAM)
-fit_y_history$estimatedRisk
-print(fit_y_history)
-plot(fit_y_history,as.matrix(fullHistoryTest[,c(2:3,5:6)]),yzTest[,1])
-
+fitY = fitFlexCoDE(L2Train, LTrain$Y,
+                   L2Validat, LValidat$Y,
+                   L2Test, LTest$Y,
+                   nIMax = 30, regressionFunction = regressionFunction.SpAM)
+fitY$estimatedRisk
+print(fitY)
+plot(fitY,L2Test, LTest$Y)
+ 
 # Fit sparse additive FlexCoDE, Z on full history
-fit_z_history=fitFlexCoDE(fullHistoryTrain[,c(2:3,5:6)],yzTrain[,2],
-                          fullHistoryValidation[,c(2:3,5:6)],yzValidation[,2],
-                          fullHistoryTest[,c(2:3,5:6)],yzTest[,2],nIMax = 30,
-                          regressionFunction = regressionFunction.SpAM)
-fit_z_history$estimatedRisk
-print(fit_z_history)
-plot(fit_z_history, as.matrix(fullHistoryTest[,c(2:3,5:6)]), yzTest[,2])
+fitZ = fitFlexCoDE(L2Train, LTrain$Z,
+                    L2Validat, LValidat$Z,
+                    L2Test, LTest$Z,
+                    nIMax = 30, regressionFunction = regressionFunction.SpAM)
+fitZ$estimatedRisk
+print(fitZ)
+plot(fitZ, L2Test, LTest$Z)
 
 # Fit X2 on X1, A1
-fit_x2_first=fitFlexCoDE(firstStageTrain[,2:3], secondStageTrain[,2],
-                         firstStageValidation[,2:3], secondStageValidation[,2],
-                         firstStageTest[,2:3], secondStageTest[,2], nIMax = 30,
-                         regressionFunction = regressionFunction.SpAM)
-fit_x2_first$estimatedRisk
-print(fit_x2_first)
-plot(fit_x2_first,firstStageTest[,2:3], secondStageTest[,2])
+L1Train = as.matrix(LTrain[,c("X1", "A1")])
+L1Validat = as.matrix(LValidat[,c("X1", "A1")])
+L1Test = as.matrix(LTest[,c("X1", "A1")])
+
+fitX2=fitFlexCoDE(L1Train, LTrain$Y,
+                  L1Validat, LValidat$Y,
+                  L1Test, LTest$Y,
+                  nIMax = 30, regressionFunction = regressionFunction.SpAM)
+fitX2$estimatedRisk
+print(fitX2)
+plot(fitX2, L1Test, LTest$X2)
 
 ########################
 # treatment assignment #
 ########################
 tau1 = c(1,2)
-tau2 = c(2,1)
-
-T1 = 1*( as.matrix(firstStageTrain[,1:2]) %*% tau1 > 0) 
-T1[T1 == 0] <- -1
-H1T = data.frame(firstStageTrain[,2], T1)
-X2.predict = predict.FlexCoDE(fit_x2_first, as.matrix(firstStageCovars[,2:3]))$z
-H2.predict= data.frame(int, X2.predict)
-T2 = 1*(as.matrix(H2.predict) %*% tau2 > 0)
-T2[T2 == 0] <- -1
-Y.predict = predict.FlexCoDE(fit_y_history, 
-            as.matrix(data.frame(X1[1:700], T1, X2.predict[1:700], T2[1:700])))$z
-
-###################### 
-## New test dataset ##
-######################
-
-# to sample from estimated CDF
-n <- 1000
-int <- rep(1, n) # intercept
-muX1 <- 1 # mean
-sigmaX1 <- 1 # covariance 
-X1 <- rnorm(n, mean = muX1, sd = sigmaX1)
-H1 <- data.frame(int, X1)
-A1 <- sample(0:1, n, replace=TRUE)
-A1[A1 == 0] <- -1
-X2Beta0 <- c(0.5, 0.75)
-X2Beta1 <- c(0.25, 0.5)
-dim(X2Beta0) <- c(2,1)
-dim(X2Beta1) <- c(2,1)
-muEpX2 = 0
-SigmaEpX2 = 1
-EpX2 <- rnorm(n, muEpX2, SigmaEpX2)
-X2 = as.matrix(H1) %*% X2Beta0 + A1 * (as.matrix(H1) %*% X2Beta1) + EpX2 
-int2 <- int
-H2 = data.frame(int2, X2)
-Ybeta0 = c(30, 3);
-Ybeta1 = c(5, -1.5);
-Zbeta0 = c(15, 1);
-Zbeta1 = c(3, -0.5);
-dim(Ybeta0) <- c(2,1)
-dim(Ybeta1) <- c(2,1)
-dim(Zbeta0) <- c(2,1)
-dim(Zbeta1) <- c(2,1)
-muEpYZ = c(0, 0);
-SigmaEpYZ = matrix(c(1 , 0.7, 0.7, 1), 2, 2)
-A2 <- sample(0:1, n, replace=TRUE)
-A2[A2 == 0] <- -1
-EpYZ =  mvrnorm(n, muEpYZ, SigmaEpYZ  )
-EpY = EpYZ[,1]
-EpZ = EpYZ[,2]
-Y = as.matrix(H2) %*% Ybeta0 + A2 * (as.matrix(H2) %*% Ybeta1) + EpY
-Z = as.matrix(H2) %*% Zbeta0 + A2 * (as.matrix(H2) %*% Zbeta1) + EpZ
-yzData <- data.frame(Y,Z)
-firstStageCovars <- data.frame(H1, A1)
-secondStageCovars <- data.frame(H2, A2)
-fullHistoryCovars <- data.frame(H2, A2, H1, A1) 
-# based on X1, A1 to estimate the density of X2
-X2.predict = predict.FlexCoDE(fit_x2_first, as.matrix(firstStageCovars[,2:3]))
-# based on X2, A2, X1, A1 to estimate the density of Y or Z
-Y.predict = predict.FlexCoDE(fit_y_history, as.matrix(fullHistoryTrain[,c(2:3,5:6)]))$z
-# based on X2, A2, X1, A1 to estimate the density of X1
-Z.predict = predict.FlexCoDE(fit_z_history, as.matrix(fullHistoryTrain[,c(2:3,5:6)]))$z
+tau2 = c(2,1,1,2)
+intTrain = int[1:round(0.7*n)]
+T1.pred = 1*( as.matrix(data.frame(intTrain, LTrain$X1)) %*% tau1 > 0) 
+T1.pred[T1 == 0] <- -1
+L1.pred = as.matrix(data.frame(LTrain$X1, T1.pred))
+X2.pred = predict.FlexCoDE(fitX2, L1.pred)$z
+X2.pred = X2.pred[1:700]
+H2.pred= data.frame(intTrain, LTrain$X1, T1.pred ,X2.pred)
+T2.pred = 1*(as.matrix(H2.pred) %*% tau2 > 0)
+T2.pred[T2.pred == 0] <- -1
+Y.pred = predict.FlexCoDE(fitY,
+         as.matrix(data.frame(LTrain$X1, T1.pred, X2.pred, T2.pred)))$z
+Z.pred = predict.FlexCoDE(fitZ,
+         as.matrix(data.frame(LTrain$X1, T1.pred, X2.pred, T2.pred)))$z
