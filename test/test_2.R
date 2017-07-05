7c8f7fe6e1d18d46de3a0d4334e44eb4ee441f81
Shuping <shuping.ruan@gmail.com>
2017-07-05 15:48:35 -0400

    update on test

--- test/test_2.R
+++ test/test_2.R
@@ -1,3 +1,7 @@
+setwd('/Users/shuping.ruan/GitHub/research-github/Multi-Stage-Constrained-Optimal-Treatment-Regimes/test/')
+
+rm(list=ls())
+
 library('FlexCoDE') # conditional density estimation
 library('stats') # univariate normal
 library('MASS') # multivariate noraml
@@ -10,31 +14,35 @@ library('nloptr')
 ########################
 
 
-values <- function(){
+#values <- function(tau, filename){
   set.seed(2018)
-  tau  <-read.table("tau.txt",header = FALSE, sep = ",", dec = ".")
+  #tau  <-read.table("tau.txt",header = FALSE, sep = ",", dec = ".")
+  tau <- c(-10,-2,-40,-10,-5,-6)
   tau1 <- tau[1:2]
   tau2 <- tau[3:6]
-  load("L1Train.Rdata")
+  load("LTrain.Rdata")
   load("fit.Rdata")
-  L1Train <- as.data.frame(L1Train)
+  LTrain <- as.data.frame(LTrain)
   # browser()
-  intTrain = rep(1,length(L1Train$X1))
-  T1.pred = 1*( as.matrix(data.frame(intTrain, L1Train$X1)) %*% t(tau1) > 0) 
-  T1.pred[T1 == 0] <- -1
-  L1.pred <- as.matrix(data.frame(L1Train$X1, T1.pred))
-  X2.pred <- predict.FlexCoDE(fitX2, L1.pred)$z
-  X2.pred <- X2.pred[1:length(L1Train$X1)]
-  H2.pred <- data.frame(intTrain, L1Train$X1, T1.pred ,X2.pred)
-  T2.pred <- 1*(as.matrix(H2.pred) %*% t(tau2) > 0)
+  intTrain = rep(1,length(LTrain$X1))
+  T1.pred = 
+    1*( as.matrix(data.frame(intTrain, LTrain$X1)) %*% t(t(tau1)) > 0) 
+  T1.pred[T1.pred == 0] <- -1
+  L1.pred <- as.matrix(data.frame(T1.pred, -1*LTrain$X1))
+  X2.pred <- predict.FlexCoDE(fitX2, L1.pred, B=length(LTrain$X1))$z
+  H2.pred <- data.frame(intTrain, X2.pred, T1.pred, LTrain$X1)
+  T2.pred <- 1*(as.matrix(H2.pred) %*% t(t(tau2)) > 0)
   T2.pred[T2.pred == 0] <- -1
-  Y.pred <- predict.FlexCoDE(fitY,
-           as.matrix(data.frame(L1Train$X1, T1.pred, X2.pred, T2.pred)))$z
-  Z.pred <- predict.FlexCoDE(fitZ,
-           as.matrix(data.frame(L1Train$X1, T1.pred, X2.pred, T2.pred)))$z
-  aveY <-  mean(Y.pred)
+  L2.pred <- data.frame(T2.pred, X2.pred, T1.pred, LTrain$X1)
+  Y.pred <- predict.FlexCoDE(fitY, as.matrix(L2.pred))$z
+  Z.pred <- predict.FlexCoDE(fitZ, as.matrix(L2.pred))$z
+  aveY <- mean(Y.pred)
   aveZ <- mean(Z.pred)
-  write(aveY, '~/GitHub/research-github/Multi-Stage-Constrained-Optimal-Treatment-Regimes/test/aveY.txt', append=FALSE)
-  write(aveZ, '~/GitHub/research-github/Multi-Stage-Constrained-Optimal-Treatment-Regimes/test/aveZ.txt', append=FALSE)
-}
-
+#  write(aveY, '~/GitHub/research-github/Multi-Stage-Constrained-Optimal-Treatment-Regimes/test/aveY_1.txt', append=FALSE)
+#  write(aveZ, '~/GitHub/research-github/Multi-Stage-Constrained-Optimal-Treatment-Regimes/test/aveZ_1.txt', append=FALSE)
+#  print(filename)
+#  save(T1.pred, X2.pred, T2.pred, Y.pred, Z.pred, file = filename)
+#  return(aveY)
+#}
+#values(c(-10,-2,-4,-10,-5,-6), 'out1.Rdata')
+#values(c(10,2,4,10,5,6), 'out2.Rdata')
---
+++ test/view.R
@@ -0,0 +1,25 @@
+load('out1.Rdata')
+T11 <- T1.pred
+X21 <- X2.pred
+T21 <- T2.pred
+Y1 <- Y.pred
+Z1 <- Z.pred
+
+rm(T1.pred)
+rm(X2.pred)
+rm(T2.pred)
+rm(Y.pred)
+rm(Z.pred)
+
+load('out2.Rdata')
+T12 <- T1.pred
+X22 <- X2.pred
+T22 <- T2.pred
+Y2 <- Y.pred
+Z2 <- Z.pred
+
+rm(T1.pred)
+rm(X2.pred)
+rm(T2.pred)
+rm(Y.pred)
+rm(Z.pred)
